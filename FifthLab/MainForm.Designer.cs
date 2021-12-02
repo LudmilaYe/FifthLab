80c5aa4ae5f82eb55a95c8b973da59980362dddf
LudmilaYe <yegupova.mila@bk.ru>
2021-12-02 11:57:31 +0800

    Создан интерфейс

--- FifthLab/MainForm.Designer.cs
+++ FifthLab/MainForm.Designer.cs
@@ -29,11 +29,67 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(12, 12);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(714, 471);
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 30;
+            // 
+            // txtLog
+            // 
+            this.txtLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLog.Location = new System.Drawing.Point(732, 12);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(209, 471);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.Text = "";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 12);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(48, 15);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Очки: 0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(953, 495);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.pbMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MiniGame";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Label lblScore;
     }
 }
